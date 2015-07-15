5f364ac0fae89ce82580dc158ee9682446b06af7
Ryo MANZOKU <manzoku@mobcast.jp>
2015-07-15 09:24:11 +0000

    React.js用のあれこれ

--- roles/dotfiles/files/emacs.d/init.el
+++ roles/dotfiles/files/emacs.d/init.el
@@ -40,11 +40,41 @@
 
 ;;Packages
 (el-get-bundle auto-complete)
-
+(el-get-bundle flycheck)
+(require 'flycheck)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  Langage
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; React.js
+(el-get-bundle web-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker)
+              (flycheck-mode))))
 
 ;; Python
 
